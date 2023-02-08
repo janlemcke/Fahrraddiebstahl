0b0d8e2cb38b04cada8f0f6125d5ac18e146ce59
Jan <janlemcke@live.de>
2023-02-08 16:01:09 +0100

    Fix ordering.

--- data/views.py
+++ data/views.py
@@ -8,11 +8,11 @@ from data.models import Report
 # Create your views here.
 class DataListView(ListView):
     model = Report
-    ordering = "created_at"
+    ordering = "createdDay"
     paginate_by = 25
 
     def get_queryset(self):
         if Report.objects.filter(createdDay__gte=datetime.date.today() - datetime.timedelta(days=1)).count() == 0:
-            return Report.objects.filter(createdDay__gte=datetime.date.today() - datetime.timedelta(days=2))
+            return Report.objects.filter(createdDay__gte=datetime.date.today() - datetime.timedelta(days=2)).order_by(self.ordering)
         else:
-            return Report.objects.filter(createdDay__gte=datetime.date.today() - datetime.timedelta(days=1))
+            return Report.objects.filter(createdDay__gte=datetime.date.today() - datetime.timedelta(days=1)).order_by(self.ordering)
